c56b66eba4baa44586ba500f359d0b7586181df9
Safiraeluna <stephanecarolline187@gmail.com>
2024-11-18 11:16:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciências',
+    'O que é a fotossíntese?',
+    'A fotossíntese é o processo pelo qual as plantas produzem energia a partir da luz solar, água e dióxido de carbono.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o maior órgão do corpo humano?',
+    'O maior órgão do corpo humano é a pele.'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é a gravidade?',
+    'A gravidade é a força que atrai os corpos em direção ao centro da Terra ou entre quaisquer dois objetos com massa.'
+)
+
+criaCartao(
+    'Ciências',
+    'Para que servem os pulmões?',
+    'Os pulmões são responsáveis pela troca de gases, absorvendo oxigênio e eliminando dióxido de carbono.'
+)
